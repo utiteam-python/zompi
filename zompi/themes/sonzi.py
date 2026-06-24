3f61e2ea06a27a773a35277ac56f07d7359c6ca0
DomeniGeco <domenigeco@gmail.com>
2026-06-24 08:11:29 +0200

    Create sonzi.py

---
+++ zompi/themes/sonzi.py
@@ -0,0 +1,14 @@
+# zompi/themes/sonzi.py
+
+from zompi.contrib import style
+
+class Theme:
+    def __init__(self):
+        self.THEME_STYLESHEET = style("""
+body {
+    color: #000;
+    background: #fff;
+    font-family: sans-serif;
+}
+""")
+      
